415a7ecf5132005e77f50dfee47c58ca5769d515
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-07-13 00:26:05 -0700

    Fixed cmdline options error in rpi2arduino-upload
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- rpi2arduino-upload.sh
+++ rpi2arduino-upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -f
 
 stty -F /dev/ttyACM0 raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
-bossac --port=ttyACM0 -U false -e -w -v -b ~/arduino-upload/sketch.bin -R
+bossac --port=ttyACM0 -e -w -v -b ~/arduino-upload/sketch.bin -R
